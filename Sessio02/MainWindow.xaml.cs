1dd569c99ac9fa830bc399f9c8f54528e6ac8c5a
nil <nvinyes.dam@institutcampalans.net>
2023-09-27 22:26:03 +0200

    inicial commit

--- Sessio02/MainWindow.xaml.cs
+++ Sessio02/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Sessio02
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
